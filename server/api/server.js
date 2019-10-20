d3f7e1a298675d4d66d7f78d7f553b69b0b9e05b
zacharyarney <zachary.arney@gmail.com>
2019-10-19 22:03:46 -0700

    GET endpoint pulling from db. errorHandler implemented.

--- server/api/server.js
+++ server/api/server.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const db = require('../data/db-config.js');
+const errorHandler = require('./middleware/errorHandlerMiddleware.js');
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+
+// Endpoints
+app.get('/', async (req, res, next) => {
+  try {
+    const notes = await db('notes').orderBy('id');
+    res.status(200).json({ notes });
+  } catch (err) {
+    return next(err);
+  }
+});
+
 
 const server = express();
 
-server.use(express.json());
-server.use(cors);
+// Error handling
+app.use(errorHandler);
 server.use(helmet);
 
-server.get('/', (req, res) => {
+module.exports = app;
   res.send('<h2> Server is connected!</h2>');
 })
