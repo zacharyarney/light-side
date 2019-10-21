290b11e94e56d41cebd4e97fcf2f28835b287a8e
zacharyarney <zachary.arney@gmail.com>
2019-10-21 00:15:49 -0700

    Note View displaying note plus link to NoteEdit

--- client/src/components/NoteView/index.js
+++ client/src/components/NoteView/index.js
@@ -1,11 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+import { apiUri } from '../../globalVariables.js';
 
-const NoteView = () => {
+const NoteView = (props) => {
+  const [note, setNote] = useState({});
+  const { match } = props;
+  const { noteTitle, noteBody, id } = note;
+
+  useEffect(() => {
+    axios
+      .get(`${apiUri}/${match.params.id}`)
+      .then((res) => {
+        setNote(res.data.note);
+      })
+      .catch((err) => console.error(err));
+  }, [match.params.id]);
+
   return (
     <div>
-      
+      <h2>{noteTitle}</h2>
+      <p>{noteBody}</p>
+      <Link to={{ pathname: '/view/:id/edit', state: { noteTitle, noteBody, id } }}>Edit</Link>
     </div>
-  )
-}
+  );
+};
 
-export default NoteView
+export default NoteView;
