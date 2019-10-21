e314cb4afe7d4b88b6dcbf3be9704eec179cc92d
zacharyarney <zachary.arney@gmail.com>
2019-10-21 00:16:31 -0700

    NoteCard displaying note

--- client/src/components/NoteCard/index.js
+++ client/src/components/NoteCard/index.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const NoteCard = () => {
-  return <div></div>;
+const NoteCard = (props) => {
+  const {
+    note: { id, noteTitle, noteBody },
+  } = props;
+
+  return (
+    <Link to={`/view/${id}`} >
+      <h2>{noteTitle}</h2>
+      <p>{noteBody}</p>
+    </Link>
+  );
 };
 
 export default NoteCard;
