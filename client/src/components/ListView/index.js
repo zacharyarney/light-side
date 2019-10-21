bfbf1a79fd02b0e01f715b8e675ff60c7a912535
zacharyarney <zachary.arney@gmail.com>
2019-10-21 00:16:56 -0700

    ListView map over NoteCards

--- client/src/components/ListView/index.js
+++ client/src/components/ListView/index.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import NoteCard from '../NoteCard';
 
-const ListView  = () => {
+import { apiUri } from '../../globalVariables.js';
+
+const ListView = () => {
+  const [notes, setNotes] = useState([]);
+
+  useEffect(() => {
+    axios.get(apiUri).then((res) => {
+      setNotes(res.data.notes);
+    }).catch(err => {
+      console.error(err);
+    });
+  }, []);
   return (
     <div>
-      
+      {notes.map((note) => {
+        return <NoteCard key={note.id} note={note} />;
+      })}
     </div>
-  )
-}
+  );
+};
 
-export default ListView 
+export default ListView;
