193d1b0b30571a46d526a937468ff10008956ff7
zacharyarney <zachary.arney@gmail.com>
2019-10-30 21:21:46 -0700

    updates messages

--- server/api/utils/messages.js
+++ server/api/utils/messages.js
@@ -1,8 +1,12 @@
 module.exports = {
-  getByIdFail: "Hmmm... I can't find that one.",
-  saved: 'Note saved!',
-  updated: 'Note updated!',
-  deleted: 'Note deleted!',
-  putNotFound: "You can't change what isn't there. How did you even get here? (Not Found)",
-  delNotFound: "You can't delete what isn't there. How did you even get here? (Not Found)"
-}
+  GET_BY_ID_FAIL: "Hmmm... I can't find that one.",
+  SAVED: 'Note saved!',
+  UPDATED: 'Note updated!',
+  DELETED: 'Note deleted!',
+  NOT_FOUND: "Hmmm... Can't find that ID.",
+  PUT_NOT_FOUND:
+    "You can't change what isn't there. How did you even get here? (ID not Found)",
+  DEL_NOT_FOUND:
+    "You can't delete what isn't there. How did you even get here? (ID not Found)",
+  SERVER_ERROR: 'Aw nuts. We messed up.',
+};
