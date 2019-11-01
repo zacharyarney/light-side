ab516c243f92a40ebfe2895236e602c7f51950a8
zacharyarney <zachary.arney@gmail.com>
2019-10-31 21:58:02 -0700

    validation still WIP

--- server/api/utils/validation.js
+++ server/api/utils/validation.js
@@ -1,16 +1,7 @@
-module.exports = (err, req, res, next) => {
-  switch (err.message) {
-    case 'CONTENT_REQUIRED':
-      return res
-        .status(400)
-        .json({ message: 'Please provide both a title and a body.' });
-    // case 'NOT_FOUND':
-    //   return res
-    //     .status(404)
-    //     .json({ message: "Hmmm... I can't find that one." });
-    default:
-      return res.status(500).json(err);
+module.exports = (title, body) => {
+  if (!title || !body) {
+    return res
+      .status(400)
+      .json({ message: 'Please provide both a title and a body.' });
   }
-
-  next();
 };
