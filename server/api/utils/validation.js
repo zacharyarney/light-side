4fab3eefa00b5f059a953861337926f7eaada162
zacharyarney <zachary.arney@gmail.com>
2019-11-17 22:40:43 -0500

    validation now returns boolean

--- server/api/utils/validation.js
+++ server/api/utils/validation.js
@@ -1,7 +1,12 @@
+// rethink error handling.
+// Maybe this should just throw an error to send to errorHandlerMiddleware.
+// Could create a new error class or object to dynamically throw errors
+// or set up different error functions.
+// food for thought...
 module.exports = (title, body) => {
   if (!title || !body) {
-    return res
-      .status(400)
-      .json({ message: 'Please provide both a title and a body.' });
+    return false;
+  } else {
+    return true;
   }
 };
