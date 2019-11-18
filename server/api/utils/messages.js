9633a17323fe15b3c42248ae6313e5474ec45861
zacharyarney <zachary.arney@gmail.com>
2019-11-17 22:38:14 -0500

    adds route-specific messages

--- server/api/utils/messages.js
+++ server/api/utils/messages.js
@@ -1,8 +1,18 @@
 module.exports = {
-  GET_BY_ID_FAIL: "Hmmm... I can't find that one.",
-  SAVED: 'Note saved!',
-  UPDATED: 'Note updated!',
-  DELETED: 'Note deleted!',
+  NOTE: {
+    GET_BY_ID_FAIL: "Hmmm... I can't find that one.",
+    SAVED: 'Note saved!',
+    UPDATED: 'Note updated!',
+    DELETED: 'Note deleted!',
+    CONTENT_REQUIRED: 'Please provide both a title and a body.',
+  },
+  COMMENT: {
+    GET_BY_ID_FAIL: "Hmmm... I can't find that one.",
+    SAVED: 'Comment saved!',
+    UPDATED: 'Comment updated!',
+    DELETED: 'Comment deleted!',
+    CONTENT_REQUIRED: 'Please provide both a title and a body.',
+  },
   NOT_FOUND: "Hmmm... Can't find that ID.",
   PUT_NOT_FOUND:
     "You can't change what isn't there. How did you even get here? (ID not Found)",
