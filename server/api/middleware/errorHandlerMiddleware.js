de242d0bee504545494c9f247723b3f6d67772fb
zacharyarney <zachary.arney@gmail.com>
2019-11-17 22:41:19 -0500

    adds validation error messages to errorHandlerMiddleware

--- server/api/middleware/errorHandlerMiddleware.js
+++ server/api/middleware/errorHandlerMiddleware.js
@@ -1,5 +1,12 @@
 const messages = require('../utils/messages.js');
-const { SERVER_ERROR, NOT_FOUND, PUT_NOT_FOUND, DEL_NOT_FOUND } = messages;
+const {
+  NOTE,
+  COMMENT,
+  SERVER_ERROR,
+  NOT_FOUND,
+  PUT_NOT_FOUND,
+  DEL_NOT_FOUND,
+} = messages;
 module.exports = (err, req, res, next) => {
   console.log(err.message);
   switch (err.message) {
@@ -9,6 +16,10 @@ module.exports = (err, req, res, next) => {
       return res.status(404).json({ PUT_NOT_FOUND });
     case 'DEL_NOT_FOUND':
       return res.status(404).json({ DEL_NOT_FOUND });
+    case 'NOTE_CONTENT_REQUIRED':
+      return res.status(400).json({ CONTENT_REQUIRED: NOTE.CONTENT_REQUIRED });
+    case 'COMMENT_CONTENT_REQUIRED':
+      return res.status(400).json({ CONTENT_REQUIRED: COMMENT.CONTENT_REQUIRED });
     default:
       return res.status(500).json({ SERVER_ERROR, err });
   }
