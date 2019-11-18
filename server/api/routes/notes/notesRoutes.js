ffdfa224eaff5b000cd2458aa76dac373354a017
zacharyarney <zachary.arney@gmail.com>
2019-11-17 22:40:06 -0500

    adds getNoteWithComments endpoint

--- server/api/routes/notes/notesRoutes.js
+++ server/api/routes/notes/notesRoutes.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const noteController = require('../../controllers/notes');
-const { getNotes, getNoteById, addNote, editNote, deleteNote } = noteController;
+const {
+  getNotes,
+  getNoteById,
+  getNoteWithComments,
+  addNote,
+  editNote,
+  deleteNote,
+} = noteController;
 
 const router = express.Router();
 
 router.get('/', getNotes);
 router.get('/:id', getNoteById);
+router.get('/:id/comments', getNoteWithComments);
 router.post('/', addNote);
 router.put('/:id', editNote);
 router.delete('/:id', deleteNote);
