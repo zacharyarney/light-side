8039bd346dbf13a0e48c7eb757140eaa2e7e2e2d
zacharyarney <zachary.arney@gmail.com>
2019-11-25 14:57:11 -0800

    Adds comments controllers

---
+++ server/api/controllers/comments/index.js
@@ -0,0 +1,91 @@
+const commentModel = require('../../models/comments');
+const messages = require('../../utils/messages.js');
+
+const {
+  COMMENT: { SAVED, UPDATED, DELETED },
+} = messages;
+
+function getComments(req, res, next) {
+  commentModel
+    .getComments()
+    .then((comments) => {
+      res.status(200).json({ comments });
+    })
+    .catch((err) => next(err));
+}
+
+function getCommentById(req, res, next) {
+  commentModel
+    .getCommentById()
+    .then((comment) => {
+      if (!comment) {
+        throw new Error('NOT_FOUND');
+      } else {
+        res.status(200).json({ comment });
+      }
+    })
+    .catch((err) => next(err));
+}
+
+function getCommentsByNote(req, res, next) {
+  commentModel
+    .getCommentsByNote()
+    .then((comments) => {
+      if (!comments) {
+        //Need a new message for no comments on a note
+        throw new Error('NOT_FOUND');
+      } else {
+        res.status(200).json({ comments });
+      }
+    })
+    .catch((err) => next(err));
+}
+
+function addComment(req, res, next) {
+  const { commentBody, user_id, note_id } = req.body;
+  const comment = { commentBody, user_id, note_id };
+
+  commentModel
+    .addComment(comment)
+    .then((id) => {
+      res.status(200).json({ message: SAVED, id });
+    })
+    .catch((err) => next(err));
+}
+
+function editComment(req, res, next) {
+  const { commentBody, user_id, note_id } = req.body;
+  const comment = { commentBody, user_id, note_id };
+  commentModel
+    .editComment(req.params.id, comment)
+    .then((id) => {
+      if (!id) {
+        throw new Error('PUT_NOT_FOUND');
+      } else {
+        res.status(200).json({ message: UPDATED, id });
+      }
+    })
+    .catch((err) => next(err));
+}
+
+function deleteComment(req, res, next) {
+  commentModel
+    .deleteComment(req.params.id)
+    .then((id) => {
+      if (!id) {
+        throw new Error('DELETE_NOT_FOUND');
+      } else {
+        res.status(200).json({ message: DELETED, id });
+      }
+    })
+    .catch((err) => next(err));
+}
+
+module.exports = {
+  getComments,
+  getCommentById,
+  getCommentsByNote,
+  addComment,
+  editComment,
+  deleteComment,
+};
