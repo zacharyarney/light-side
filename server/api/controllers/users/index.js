6fe6d42706ac09a4989579b6caa4531d40e9c513
zacharyarney <zachary.arney@gmail.com>
2019-11-25 14:08:08 -0800

    formatting

---
+++ server/api/controllers/users/index.js
@@ -0,0 +1,105 @@
+const userModel = require('../../models/users');
+const messages = require('../../utils/messages.js');
+
+const {
+  NOTE: { SAVED, UPDATED, DELETED },
+} = messages;
+
+function getUsers(req, res, next) {
+  userModel
+    .getUsers()
+    .then((users) => {
+      res.status(200).json({ users });
+    })
+    .catch((err) => next(err));
+}
+
+function getUserById(req, res, next) {
+  userModel
+    .getUserById()
+    .then((user) => {
+      if (!user) {
+        throw new Error('NOT_FOUND');
+      } else {
+        res.status(200).json({ user });
+      }
+    })
+    .catch((err) => next(err));
+}
+
+function getUserWithNotes(req, res, next) {
+  userModel
+    .getUserWithNotes()
+    .then((user) => {
+      if (!user) {
+        throw new Error('NOT_FOUND');
+      } else {
+        res.status(200).json({ user });
+      }
+    })
+    .catch((err) => next(err));
+}
+
+function getUserWithComments(req, res, next) {
+  userModel
+    .getUserWithComments()
+    .then((user) => {
+      if (!user) {
+        throw new Error('NOT_FOUND');
+      } else {
+        res.status(200).json({ user });
+      }
+    })
+    .catch((err) => next(err));
+}
+
+function addUser(req, res, next) {
+  const { username } = req.body;
+  const user = { username };
+
+  userModel
+    .addUser(user)
+    .then((id) => {
+      res.status(200).json({ message: SAVED, id });
+    })
+    .catch((err) => next(err));
+}
+
+//TODO need to change up req.params to accomodate new routes
+function editUser(req, res, next) {
+  const { username } = req.body;
+
+  userModel
+    .editUser(req.params.id, username)
+    .then((id) => {
+      if (!id) {
+        throw new Error('PUT_NOT_FOUND');
+      } else {
+        res.status(200).json({ message: UPDATED, id });
+      }
+    })
+    .catch((err) => next(err));
+}
+
+function deleteUser(req, res, next) {
+  userModel
+    .deleteUser(req.params.id)
+    .then((id) => {
+      if (!id) {
+        throw new Error('DEL_NOT_FOUND');
+      } else {
+        res.status(200).json({ message: DELETED, id });
+      }
+    })
+    .catch((err) => next(err));
+}
+
+module.exports = {
+  getUsers,
+  getUserById,
+  getUserWithNotes,
+  getUserWithComments,
+  addUser,
+  editUser,
+  deleteUser,
+};
--- server/api/models/users/index.js
+++ server/api/models/users/index.js
@@ -6,41 +6,41 @@ function getUsers() {
 
 function getUserById(id) {
   return db('users')
-    .where({id})
+    .where({ id })
     .first();
 }
 
 function getUserWithNotes(id) {
   return db('users')
-    .where({id})
+    .where({ id })
     .first()
-    .join('notes', {'notes.user_id': 'user.id'});
+    .join('notes', { 'notes.user_id': 'user.id' });
 }
 
 function getUserWithComments(id) {
   return db('users')
-    .where({id})
+    .where({ id })
     .first()
-    .join('comments', {'comments.user_id': 'user.id'});
+    .join('comments', { 'comments.user_id': 'user.id' });
 }
 
 function addUser(user) {
   db('users')
     .returning('id')
-    .insert({user});
+    .insert({ user });
 }
 
 function editUser(id, username) {
   db('users')
     .returning('id')
-    .where({id})
-    .update({username})[0];
+    .where({ id })
+    .update({ username })[0];
 }
 
 function deleteUser(id) {
   db('users')
     .returning('id')
-    .where({id})
+    .where({ id })
     .delete()[0];
 }
 
