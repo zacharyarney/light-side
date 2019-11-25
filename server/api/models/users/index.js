d28745d15705fe91cc40662ea63adc9c33b75250
zacharyarney <zachary.arney@gmail.com>
2019-11-25 14:06:30 -0800

    Adds user models

--- server/api/models/users/index.js
+++ server/api/models/users/index.js
@@ -6,28 +6,50 @@ function getUsers() {
 
 function getUserById(id) {
   return db('users')
-    .where({ id })
+    .where({id})
     .first();
+}
+
+function getUserWithNotes(id) {
+  return db('users')
+    .where({id})
+    .first()
+    .join('notes', {'notes.user_id': 'user.id'});
+}
+
+function getUserWithComments(id) {
+  return db('users')
+    .where({id})
+    .first()
+    .join('comments', {'comments.user_id': 'user.id'});
 }
 
 function addUser(user) {
   db('users')
     .returning('id')
-    .insert({ user });
+    .insert({user});
 }
 
 function editUser(id, username) {
   db('users')
     .returning('id')
-    .where({ id })
-    .update({ username })[0];
+    .where({id})
+    .update({username})[0];
 }
 
 function deleteUser(id) {
   db('users')
     .returning('id')
-    .where({ id })
+    .where({id})
     .delete()[0];
 }
 
-module.exports = { getUsers, getUserById, addUser, editUser, deleteUser };
+module.exports = {
+  getUsers,
+  getUserById,
+  getUserWithNotes,
+  getUserWithComments,
+  addUser,
+  editUser,
+  deleteUser,
+};
