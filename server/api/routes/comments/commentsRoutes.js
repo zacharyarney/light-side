0c32a9769de69901049a8d3982690834075ea46c
zacharyarney <zachary.arney@gmail.com>
2019-11-25 15:16:22 -0800

    adds comments routes

---
+++ server/api/routes/comments/commentsRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const commentsController = require('../../controllers/comments');
+const {
+  getComments,
+  getCommentById,
+  getCommentsByNote,
+  addComment,
+  editComment,
+  deleteComment,
+} = commentsController;
+
+const router = express.Router();
+
+router.get('/', getComments);
+router.get('/:id', getCommentById);
+router.get('/:noteId', getCommentsByNote);
+router.post('/', addComment);
+router.put('/:id', editComment);
+router.delete('/:id', deleteComment);
+
+module.exports = router;
+
+
