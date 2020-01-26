1dc645f9389313b5ee545e4ebade517e3ba02014
zacharyarney <zachary.arney@gmail.com>
2020-01-26 00:27:42 -0800

    Adds resolvers.js test resolvers

---
+++ graphql-server/graphql/resolvers/resolvers.js
@@ -0,0 +1,38 @@
+let count = 0;
+let posts = [];
+
+const resolvers = {
+  Query: {
+    description: () =>
+      'Getting started with GraphQL. THIS IS STILL A TEST',
+    posts: () => posts,
+    post: (_, args) => posts.find((post) => post.id === args.input.id),
+  },
+  Mutation: {
+    createDraft: (_, args) => {
+      const post = {
+        id: `post_${count++}`,
+        title: args.input.title,
+        content: args.input.content,
+        published: false,
+      };
+      posts.push(post);
+      console.log(posts);
+      return post;
+    },
+    deletePost: (_, args) => {
+      const postIndex = posts.findIndex((post) => post.id === args.input.id);
+      if (postIndex > -1) {
+        const deletedPost = posts.splice(postIndex, 1);
+        return deletedPost;
+      }
+    },
+    publish: (_, args) => {
+      const postIndex = posts.findIndex((post) => post.id === args.input.id);
+      posts[postIndex].published = true;
+      return posts[postIndex];
+    },
+  },
+};
+
+module.exports = resolvers;
