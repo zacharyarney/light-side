ae036f84ffdce96015aa635dfc4e8278bc39c49e
zacharyarney <zachary.arney@gmail.com>
2020-01-26 20:01:23 -0800

    simplifies schema and resolvers

--- graphql-server/graphql/resolvers/resolvers.js
+++ graphql-server/graphql/resolvers/resolvers.js
@@ -6,29 +6,29 @@ const resolvers = {
     description: () =>
       'Getting started with GraphQL. THIS IS STILL A TEST',
     posts: () => posts,
-    post: (_, { input }) => posts.find((post) => post.id === input.id),
+    post: (parent, { id }) => posts.find((post) => post.id === id),
   },
   Mutation: {
-    createDraft: (_, { input }) => {
+    createDraft: (parent, { title, content }) => {
       const post = {
         id: `post_${count++}`,
-        title: input.title,
-        content: input.content,
+        title: title,
+        content: content,
         published: false,
       };
       posts.push(post);
       console.log(posts);
       return post;
     },
-    deletePost: (_, { input }) => {
-      const postIndex = posts.findIndex((post) => post.id === input.id);
+    deletePost: (parent, { id }) => {
+      const postIndex = posts.findIndex((post) => post.id === id);
       if (postIndex > -1) {
         const deletedPost = posts.splice(postIndex, 1);
         return deletedPost;
       }
     },
-    publish: (_, { input }) => {
-      const postIndex = posts.findIndex((post) => post.id === input.id);
+    publish: (parent, { id }) => {
+      const postIndex = posts.findIndex((post) => post.id === id);
       posts[postIndex].published = true;
       return posts[postIndex];
     },
