7ede9f1c4d36dad565b667be89b38e8e91f7870c
zacharyarney <zachary.arney@gmail.com>
2020-02-01 13:08:46 -0800

    renames root folder to 'light_side_django_root'

--- light-side-django/light_side_django_root/light_side_django/asgi.py
+++ light-side-django/light_side_django_root/light_side_django/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for light_side_django project.
+ASGI config for light_side_django_root project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'light_side_django.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'light_side_django_root.settings')
 
 application = get_asgi_application()
--- light-side-django/light_side_django_root/light_side_django/urls.py
+++ light-side-django/light_side_django_root/light_side_django/urls.py
@@ -1,4 +1,4 @@
-"""light_side_django URL Configuration
+"""light_side_django_root URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
