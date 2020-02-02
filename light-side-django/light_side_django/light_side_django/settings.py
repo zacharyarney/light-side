1bc8b024b375ff1794b1626899e5655d8cc12d30
zacharyarney <zachary.arney@gmail.com>
2020-02-01 18:52:27 -0800

    moving env variables to .env

--- light-side-django/light_side_django/light_side_django/settings.py
+++ light-side-django/light_side_django/light_side_django/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from decouple import config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -19,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!rpy4l+0k2(i6$&n)dh4peg!-2vdd1^p)zjl+ppo)$uii#v%sy'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
