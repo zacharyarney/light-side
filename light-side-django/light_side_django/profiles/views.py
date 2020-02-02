f04baeb31e23be3a1d66923e5c3ee5c5caa389ab
zacharyarney <zachary.arney@gmail.com>
2020-02-02 12:36:32 -0800

    Sets up user view sets and serializers

--- light-side-django/light_side_django/profiles/views.py
+++ light-side-django/light_side_django/profiles/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.contrib.auth.models import User
+from rest_framework.viewsets import generics
 
-# Create your views here.
+from light_side_django.profiles.serializers import UserSerializer
+
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserDetailView(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
