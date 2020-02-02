e50926d4569b62561ef1658c89843904631c97dc
zacharyarney <zachary.arney@gmail.com>
2020-02-01 18:49:44 -0800

    adds timestamps and PersonalNote sub-model

--- light-side-django/light_side_django/notes/models.py
+++ light-side-django/light_side_django/notes/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Note(models.Model):
@@ -6,3 +7,9 @@ class Note(models.Model):
     # id = models.AutoField(primary_key=True)
     noteTitle = models.CharField(max_length=255, unique=True)
     noteBody = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class PersonalNote(Note):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
