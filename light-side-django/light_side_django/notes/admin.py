1e0bfdad099c9db96da78d2ca88ef98e91119c0d
zacharyarney <zachary.arney@gmail.com>
2020-02-01 18:49:56 -0800

    adds models to admin view

--- light-side-django/light_side_django/notes/admin.py
+++ light-side-django/light_side_django/notes/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Note, PersonalNote
+
+
+class NoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at', 'id')
+
+
+class PersonalNoteAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at', 'id')
+
+
 # Register your models here.
+admin.site.register(Note, NoteAdmin)
+admin.site.register(PersonalNote, PersonalNoteAdmin)
