fa5e1e85ed90d707d8814ad8120d84160b03b3ad
zacharyarney <zachary.arney@gmail.com>
2020-02-01 17:36:49 -0800

    id is autogenerated by Django--comments out id

--- light-side-django/light_side_django/notes/models.py
+++ light-side-django/light_side_django/notes/models.py
@@ -2,6 +2,7 @@ from django.db import models
 
 
 class Note(models.Model):
-    id = models.AutoField(primary_key=True)
+    # automatic id field is automatically added by Django
+    # id = models.AutoField(primary_key=True)
     noteTitle = models.CharField(max_length=255, unique=True)
     noteBody = models.TextField(blank=True)
