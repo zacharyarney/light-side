0e833557fad7def31ab0b44101c228a2549b84d8
zacharyarney <zachary.arney@gmail.com>
2020-02-06 16:33:49 -0800

    the purge--rebuilding

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import { Security, ImplicitCallback } from '@okta/okta-react';
 import { Route } from 'react-router-dom';
 import './App.css';
 import Sidebar from './components/Sidebar';
-import ListView from './components/ListView';
+import ListView from './components/ListView/ListView';
 import NoteView from './components/NoteView';
 import NoteCreate from './components/NoteCreate';
 import NoteEdit from './components/NoteEdit';
@@ -12,12 +12,8 @@ import NoteDelete from './components/NoteDelete';
 function App() {
   return (
     <div className="App">
-      <Route path="/" component={Sidebar} />
-      <Route exact path="/" component={ListView} />
-      <Route exact path="/view/:id" component={NoteView} />
-      <Route path="/write" component={NoteCreate} />
-      <Route path="/view/:id/edit" component={NoteEdit} />
-      <Route path="/view/:id/delete" component={NoteDelete} />
+      <Route exact path="/home" component={ListView} />
+      <Route exact path="/login" />
     </div>
   );
 }
---
+++ client/src/components/ListView/ListView.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect, useState } from 'react';
+
+const ListView = () => {
+  return (
+    <div className="list-view">
+      <h1>LightSide</h1>
+    </div>
+  );
+};
+
+export default ListView;
