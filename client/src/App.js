8610c70f092668b8463e98f70e287c92ab4c0cab
zacharyarney <zachary.arney@gmail.com>
2020-02-07 17:28:42 -0800

    Sets up App with ImplicitCallback, LoginView, SecureRoute for HomeView

--- client/src/App.js
+++ client/src/App.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { Security, ImplicitCallback } from '@okta/okta-react';
+import { SecureRoute, ImplicitCallback } from '@okta/okta-react';
 import { Route } from 'react-router-dom';
 import './App.css';
-import Sidebar from './components/Sidebar';
-import ListView from './components/ListView/ListView';
-import NoteView from './components/NoteView';
-import NoteCreate from './components/NoteCreate';
-import NoteEdit from './components/NoteEdit';
-import NoteDelete from './components/NoteDelete';
+import config from './app.config';
+import HomeView from './components/HomeView/HomeView';
+import LoginView from './components/LoginView/LoginForm';
 
 function App() {
+  console.log('url in App: ', config)
   return (
     <div className="App">
-      <Route exact path="/home" component={ListView} />
-      <Route exact path="/login" />
+      <SecureRoute exact path="/home" component={HomeView} />
+      <Route path="/login" render={() => <LoginView baseUrl={config.url} />} />
+      <Route exact path="/implicit/callback" component={ImplicitCallback} />
     </div>
   );
 }
