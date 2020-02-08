0f35d8bd44d239bc8d5769cf7d78cd84c63360cc
zacharyarney <zachary.arney@gmail.com>
2020-02-07 17:22:59 -0800

    adds url property to app.config.js

--- client/src/app.config.js
+++ client/src/app.config.js
@@ -1,4 +1,5 @@
 const config = {
+  url: process.env.REACT_APP_OKTA_URL,
   issuer: process.env.REACT_APP_OKTA_ISSUER,
   redirectUri: window.location.origin + '/login',
   clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
