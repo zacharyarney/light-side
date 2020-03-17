040a6303cc24c20273e93fd4df0388544a0cc7cf
zacharyarney <zachary.arney@gmail.com>
2020-03-16 22:26:15 -0700

    adds index.tsx

---
+++ client/src/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+import { Hello } from './components/Hello';
+
+ReactDOM.render(<App />, document.getElementById('root'));
