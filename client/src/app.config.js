29d4382703ba15f4e9551df42552c483bb02a88c
zacharyarney <zachary.arney@gmail.com>
2020-03-16 19:36:16 -0700

    the CRA culling

--- client/src/app.config.js
+++
@@ -1,9 +0,0 @@
-const config = {
-  url: process.env.REACT_APP_OKTA_URL,
-  issuer: process.env.REACT_APP_OKTA_ISSUER,
-  redirectUri: window.location.origin + '/login',
-  clientId: process.env.REACT_APP_OKTA_CLIENT_ID,
-  pkce: true,
-};
-
-export default config;
