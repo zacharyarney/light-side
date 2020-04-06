c4acefe206a87eb4e3f9abcf1eafce8f544ded4d
zacharyarney <zachary.arney@gmail.com>
2020-04-06 00:38:56 -0700

    Adds ability to set auth headers for HTTP requests

--- client/src/components/Auth/Auth.tsx
+++ client/src/components/Auth/Auth.tsx
@@ -1,19 +1,45 @@
 import React, { createContext, useState, useEffect } from 'react';
+import axios from 'axios';
 import app, { UserContext } from '../../utils/firebase';
 
 interface ProviderParams {
   children: React.ReactNode;
 }
 
-export const FirebaseContext = createContext<UserContext>({ currentUser: undefined });
+interface UserContextProperties {
+  currentUser: firebase.User | null | undefined;
+}
+
+type CurrentUserType = firebase.User | null;
+type UserType = firebase.User | null;
+
+export const FirebaseContext = createContext<UserContext>({
+  currentUser: undefined,
+});
 
 function FirebaseProvider({ children }: ProviderParams) {
   // Maybe figure out typing for useState here to avoid the 'any'
   // 'firebase.User' didn't work
-  const [currentUser, setCurrentUser] = useState<any>(null);
+  const [currentUser, setCurrentUser] = useState<CurrentUserType>(null);
+  const [pending, setPending] = useState<boolean>(true);
 
   useEffect(() => {
-    app.auth().onAuthStateChanged(setCurrentUser);
+    async function httpAuthHeaders(user: UserType) {
+      if (user) {
+        const idToken = await app.auth().currentUser?.getIdToken()
+        axios.defaults.headers.common['Authorization'] = idToken;
+      }
+    }
+    const unsubscribe = app.auth().onAuthStateChanged(user => {
+      httpAuthHeaders(user);
+      if (user) {
+      setCurrentUser(user);
+      setPending(false);
+      } else {
+        setCurrentUser(null)
+      }
+    })
+    return () => unsubscribe();
   }, []);
 
   return (
